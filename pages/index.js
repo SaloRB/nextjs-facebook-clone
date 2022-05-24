17313da4ca1adb89b50fd6bcf37020764b339d53
Salomon Romano <salo_romano@yahoo.com>
2022-05-24 11:17:49 -0500

    Change to js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Facebook 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
+
+export default Home
